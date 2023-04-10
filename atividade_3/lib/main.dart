0f941791b27086a11e1209c99c75716bb43a2071
Japagabriel <gcanutosantos@gmail.com>
2023-04-09 23:30:02 -0300

    A atividade 10 e 11 estão alinhadas em uma só

--- atividade_3/lib/main.dart
+++ atividade_3/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'meuApp.dart';
+
+//  As atividade 10 e 11 estão contidas nessa mesmo programa, contento uma tabela de informações e e o scroll funcional.
+
 void main() {
   runApp(MeuApp());
 }
